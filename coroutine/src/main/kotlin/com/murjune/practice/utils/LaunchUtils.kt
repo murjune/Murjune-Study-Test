aeb9ba570e5fa79c57c306d4ee853bf30f319265
murjune <sis9221302@gmail.com>
2024-06-24 16:31:49 +0900

    utils: launchWithElapsedTime
    launch 경과 시간 logging 하는 함수

--- coroutine/src/main/kotlin/com/murjune/practice/utils/LaunchUtils.kt
+++ coroutine/src/main/kotlin/com/murjune/practice/utils/LaunchUtils.kt
@@ -19,5 +19,28 @@ fun CoroutineScope.launchWithName(name: String, job: Job? = null, block: suspend
             log("$name 종료")
         }
     }
+}
 
+fun CoroutineScope.launchWithElapsedTime(
+    name: String,
+    job: Job? = null,
+    block: suspend CoroutineScope.() -> Unit
+): Job {
+    val newJob = if (job == null) {
+        CoroutineName(name)
+    } else {
+        CoroutineName(name) + job
+    }
+    var startTime = 0L
+    return launch(newJob) {
+        startTime = System.currentTimeMillis()
+        log("$name 시작")
+        block()
+    }.apply {
+        invokeOnCompletion {
+            val endTime = System.currentTimeMillis()
+            log("$name 종료")
+            log("$name 소요시간: ${endTime - startTime}ms")
+        }
+    }
 }
