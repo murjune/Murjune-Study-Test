dcda9580e38699cd03afbace5447d55ebf7038a3
murjune <sis9221302@gmail.com>
2024-09-29 17:50:12 +0900

    [JAVA] IntStreamTest

---
+++ java-practice/src/test/java/poke/rogue/java/practice/IntStreamTest.java
@@ -0,0 +1,22 @@
+package poke.rogue.java.practice;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+class IntStreamTest {
+    @Test
+    void testIntStream() {
+        //given
+        int n = 5;
+        List<Integer> li = new ArrayList<>();
+        //when
+        IntStream.range(0, n).forEach(li::add);
+        //then
+        assertThat(li).containsExactly(0, 1, 2, 3, 4);
+    }
+}
