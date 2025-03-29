3ce80faef374bc150a88e64e25339a40b7f16f79
murjune <sis9221302@gmail.com>
2025-03-29 22:00:28 +0900

    fix(buid-logic): jvmLibraryPlugin 수정

--- build-logic/convention/src/main/kotlin/JvmLibraryPlugin.kt
+++ build-logic/convention/src/main/kotlin/JvmLibraryPlugin.kt
@@ -1,14 +1,20 @@
 import com.murjune.practice.plugins.configureKotlinJvm
+import com.murjune.practice.plugins.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.dependencies
 
 class JvmLibraryPlugin : Plugin<Project> {
-    override fun apply(target: Project) {
-        with(target) {
-            with(pluginManager) {
-                apply("org.jetbrains.kotlin.jvm")
-            }
-            configureKotlinJvm()
+    override fun apply(target: Project): Unit = with(target) {
+        apply(plugin = "org.jetbrains.kotlin.jvm")
+
+        configureKotlinJvm()
+
+        dependencies {
+            "implementation"(libs.findLibrary("kotlinx.coroutines.core").get())
+            "implementation"(libs.findLibrary("kotlinx.datetime").get())
+            "testImplementation"(libs.findBundle("unit.test").get())
         }
     }
 }
