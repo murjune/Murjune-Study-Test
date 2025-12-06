591cd67ed8c29576d71b752fbefc92bfaebc49bf
murjune <sis9221302@gmail.com>
2025-12-06 21:18:27 +0900

    update: kotlin-practice build.gradle

--- kotlin-practice/build.gradle.kts
+++ kotlin-practice/build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     alias(libs.plugins.murjune.jvm.library)
     alias(libs.plugins.murjune.unit.test)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 group = "com.murjune.practice.kotlin.practice"
 
 dependencies {
     implementation("com.github.javafaker:javafaker:1.0.2")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.23")
+    implementation(libs.kotlin.reflect)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.gson)
 }
